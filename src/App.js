d3ac46474735ba8a6d3038b307f0e080268a56db
Diwan111 <69912582+Diwan111@users.noreply.github.com>
2022-02-16 16:08:29 +0530

    use of nested Route and Outlet

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { About } from './Components/Page/About';
 import { Contact } from './Components/Page/Contact';
 import { Home } from './Components/Page/Home';
-import { Navbar } from './Components/Navbar';
+import { Layout } from './Components/Layout';
 import { Post } from './Components/Page/Post';
-import Dashboard from './Components/Page/Dashboard';
-import Login from './Components/Page/Login';
-import Logout from './Components/Page/Logout';
 
 function App() {
 
-  const [isLogged, setIsLogged] = useState(true);
-  let data = {
-    st : 'User not logged in'
-  }
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<h1>Hello Anish Diwan</h1>} /> */}
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/post/:category/:id' element={<Post />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/logout' element={ <Logout /> } />
-          <Route path='/dashboard' element={isLogged ? <Dashboard /> : <Navigate to='/login' state={data} replace /> } />
-          <Route path='*' element={<h1 style={{ textAlign: 'center' }}>Error 404 Page not Found</h1>} />
+          <Route path='/' element={<Layout />} >
+            <Route path='about' element={ <About /> } />
+          </Route>
         </Routes>
-        <Navbar />
       </BrowserRouter>
     </>
   );
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
-export const Navbar = () => {
+export const Layout = () => {
     return (
         <>
-            {/* <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-    </ul> */}
-
+            <h1 style={{textAlign:'center'}}>React Router v6</h1>
             <ul className='nav-link'>
                 <li><NavLink to="/" style={({ isActive }) => { return { backgroundColor: isActive ? 'red' : '' } }}>Home</NavLink></li>
                 <li><NavLink to="/about" style={({ isActive }) => { return { backgroundColor: isActive ? 'red' : '' } }}>About</NavLink></li>
                 <li><NavLink to="/contact" style={({ isActive }) => { return { backgroundColor: isActive ? 'red' : '' } }}>Contact</NavLink></li>
                 <li><NavLink to="/post/mobile/1" style={({ isActive }) => { return { backgroundColor: isActive ? 'red' : '' } }}>Post</NavLink></li>
             </ul>
+            <Outlet />
         </>
     )
 }
