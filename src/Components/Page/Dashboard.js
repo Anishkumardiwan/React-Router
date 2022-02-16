dd1ff7f70db75b8453b8edcf07ea6aeab04a9435
Diwan111 <69912582+Diwan111@users.noreply.github.com>
2022-02-16 10:50:49 +0530

    use of useNavigate

--- src/Components/Page/Dashboard.js
+++ src/Components/Page/Dashboard.js
@@ -3,14 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     let navigate = useNavigate();
+    let data = { name: 'Anish'};
     return (
         <>
             <h1 style={{textAlign:'center'}}>Dashboard Page</h1>
             <button style={{textAlign:'center'}} onClick={()=>{
-                navigate("/logout")
+                navigate("/logout" , {state:data})
             }}>Logout</button>
         </>
     )
 }
 
 export default Dashboard;
+
